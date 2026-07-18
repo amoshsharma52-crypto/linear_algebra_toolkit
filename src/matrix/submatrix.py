f27e41d25ec4b6d5a854bf416ae3d9bc3d87d276
amosh <amoshsharma52@gmail.com>
2026-07-18 16:25:39 +0530

    submatrix tested

--- src/matrix/submatrix.py
+++ src/matrix/submatrix.py
@@ -1,2 +1,13 @@
 def submatrix(matrix, row_to_remove, col_to_remove):
-    pass
+    result = []
+
+    for i, row in enumerate(matrix):
+
+        if i == row_to_remove:
+            continue
+
+        new_row = row[:col_to_remove] + row[col_to_remove + 1:]
+
+        result.append(new_row)
+
+    return result
